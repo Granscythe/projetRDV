740faac5c99cffb396a9b3b90bba6d3a09292c8a
rene <benjamin.rene@viacesi.fr>
2021-08-12 10:33:16 +0200

    classes benji

---
+++ src/main/java/sopra/formation/model/CreneauUnitaire.java
@@ -0,0 +1,143 @@
+package sopra.formation.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+@Entity
+@DiscriminatorValue("CreneauUnitaire")
+public class CreneauUnitaire {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Version
+	private int version;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="Date de debut unitaire")
+	private Date dtDebutUnitaire;
+	
+	@Column(name = "Duree")
+	private Integer duree;
+	
+	@Column(name = "Statut")
+	private boolean statut;
+	
+	@ManyToOne
+	@JoinColumn(name="RDV")
+	private RDV rdv;
+	
+	@ManyToOne
+	@JoinColumn(name="CreneauPraticien")
+	private CreneauPraticien creneauPraticien;
+	
+
+	public CreneauUnitaire() {
+		super();
+	}
+
+
+	public CreneauUnitaire(Long id, int version, Date dtDebutUnitaire, Integer duree, boolean statut, RDV rdv,
+			CreneauPraticien creneauPraticien) {
+		this.id = id;
+		this.version = version;
+		this.dtDebutUnitaire = dtDebutUnitaire;
+		this.duree = duree;
+		this.statut = statut;
+		this.rdv = rdv;
+		this.creneauPraticien = creneauPraticien;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public int getVersion() {
+		return version;
+	}
+
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+
+	public Date getDtDebutUnitaire() {
+		return dtDebutUnitaire;
+	}
+
+
+	public void setDtDebutUnitaire(Date dtDebutUnitaire) {
+		this.dtDebutUnitaire = dtDebutUnitaire;
+	}
+
+
+	public Integer getDuree() {
+		return duree;
+	}
+
+
+	public void setDuree(Integer duree) {
+		this.duree = duree;
+	}
+
+
+	public boolean isStatut() {
+		return statut;
+	}
+
+
+	public void setStatut(boolean statut) {
+		this.statut = statut;
+	}
+
+
+	public RDV getRdv() {
+		return rdv;
+	}
+
+
+	public void setRdv(RDV rdv) {
+		this.rdv = rdv;
+	}
+
+
+	public CreneauPraticien getCreneauPraticien() {
+		return creneauPraticien;
+	}
+
+
+	public void setCreneauPraticien(CreneauPraticien creneauPraticien) {
+		this.creneauPraticien = creneauPraticien;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CreneauUnitaire [id=" + id + ", version=" + version + ", dtDebutUnitaire=" + dtDebutUnitaire
+				+ ", duree=" + duree + ", statut=" + statut + ", creneauPraticien=" + creneauPraticien + "]";
+	}
+
+	
+	
+}
